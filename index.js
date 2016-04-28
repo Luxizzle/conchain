89795837634401b3c1625275b3e0eb168acd5d4a
Lux <luxizzle@gmail.com>
2016-04-28 23:44:22 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+// Chaining dirs together for a nice workflow
+// Created by Lux Pandora
+
+var root = require('app-root-path'),
+    fs = require('fs')
+
+module.exports = function(options, app, dirs) {
+    options = options || {}
+
+    var _options = {
+        base: '',
+        sep: '/',
+        log: {
+            base: true,
+            dir: false,
+            file: false
+        }
+    }
+
+    for (var o in options) {
+        _options[o] = options[o];
+    }
+
+    var context = {_app: app}
+
+    if (_options.log.base) console.log('-', _options.base)
+    dirs.forEach(function(cur, index) {
+        if (_options.log.dir) console.log('--', cur)
+
+        var curdir = root + '/' + _options.base + _options.sep + cur
+
+        //get the files in the directory
+        var files = fs.readdirSync(curdir)
+
+        //create a place for it to save
+        context[cur] = {}
+
+        //console.log(files)
+        files.forEach(function(curfile) {
+            var name = curfile.split('.')[0]
+            if (!context[cur][name]) {
+                context[cur][name] = {}
+            }
+
+            var mod = require(curdir + '/' + curfile)(context[cur][name], context, app)
+            if (_options.log.file) console.log('---', curfile)
+
+        })
+    })
+
+}
+    
